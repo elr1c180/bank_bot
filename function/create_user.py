75620192f19c372b00314869170d1deb1ed9bc2d
Anton Elesin <elr1c.edw180@gmail.com>
2024-08-12 13:13:57 +0300

    new_request

---
+++ function/create_user.py
@@ -0,0 +1,11 @@
+import aiohttp
+
+
+ROOT_URL = 'http://217.196.98.13:9000/'
+
+async def create_user(chat_id, username, first_name):
+    user_data = {"chat_id": chat_id, "username": username, "name": first_name}
+    async with aiohttp.ClientSession() as session:
+        async with session.post(ROOT_URL + 'create_user/', data=user_data) as resp:
+            print(resp.status)
+            print(await resp.text())
